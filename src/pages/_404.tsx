b8b1744d13c3de07737d5d1468d2e1739f62ffaf
emeraldsanto <yanick.belanger@yahoo.com>
2020-10-13 21:02:42 -0400

    404 page

--- src/pages/_404.tsx
+++
@@ -1,9 +0,0 @@
-import { Page } from "@components/page/page.component";
-import { NextPage } from "next";
-
-const NotFound: NextPage = () => {
-	// TODO: Sweet 404 page
-	return <Page></Page>;
-};
-
-export default NotFound;
