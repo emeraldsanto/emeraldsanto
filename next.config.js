b1bb4aa2767cd997a1eb543291538e1c01e82d3e
emeraldsanto <yanick.belanger@yahoo.com>
2020-11-14 12:50:19 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 const { locales, defaultLocale } = require('./i18n.json')
 
-const localeSubpaths = {
-	en: "en",
-	fr: "fr",
-};
-
 module.exports = {
-	i18n: { locales, defaultLocale },
-	publicRuntimeConfig: { localeSubpaths }
+	i18n: { locales, defaultLocale }
 };
