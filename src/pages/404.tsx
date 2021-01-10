b558f7bb295389c203880211bd937f7e0568d42f
emeraldsanto <yanick.belanger@yahoo.com>
2021-01-10 13:20:39 -0500

    404 page styled-jsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,28 +1,70 @@
 import { Button } from "@components/button/button.component";
 import { Page } from "@components/page/page.component";
-import styles from "@styles/404.module.scss";
+import { combineClassNames } from '@lib/design';
 import { NextPage } from "next";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
+import { Fragment } from 'react';
+import css from 'styled-jsx/css';
 
 const NotFound: NextPage = () => {
 	const { t } = useTranslation();
+	const buttonStyles = getButtonStyles();
 
 	return (
-		<Page>
-			<div className={styles.container}>
-				<h1 className={styles.status}>404</h1>
-				<p className={styles.title}>{t("404:title")}</p>
-				<p className={styles.catch}>{t("404:catch")}</p>
+    <Fragment>
+      <Page>
+        <div className="container">
+          <h1 className="status">404</h1>
+          <p className="title">{t("404:title")}</p>
+          <p className="catch">{t("404:catch")}</p>
 
-				<Link href="/">
-					<Button className={styles.button}>
-						<p>{t("404:cta")}</p>
-					</Button>
-				</Link>
-			</div>
-		</Page>
-	);
+          <Link href="/">
+            <Button className={combineClassNames("button", buttonStyles)}>
+              <p>{t("404:cta")}</p>
+            </Button>
+          </Link>
+        </div>
+      </Page>
+
+      <style jsx>{`
+        .container {
+          width: 500px;
+        }
+
+        .status {
+          margin-bottom: 10px;
+        }
+
+        .title {
+          font-size: 1.5em;
+        }
+
+        .catch {
+          margin: 20px 0 30px 0;
+          text-align: justify;
+        }
+
+        @media only screen and (max-width: 625px) {
+          .container {
+            width: 100%;
+          }
+        }
+      `}</style>
+
+			{buttonStyles.styles}
+    </Fragment>
+  );
 };
 
 export default NotFound;
+
+function getButtonStyles() {
+	return css.resolve`
+    @media only screen and (max-width: 625px) {
+      .button {
+        width: 100%;
+      }
+    }
+  `;
+}
