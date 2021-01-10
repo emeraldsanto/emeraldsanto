c09bec7df35bec75d0ab0cd031da44707cdead81
emeraldsanto <yanick.belanger@yahoo.com>
2021-01-10 12:17:22 -0500

    Migrated input to styled-jsx

--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -1,23 +1,58 @@
+import { useTheme } from '@contexts/theme/theme-context';
 import {
 	DetailedHTMLProps,
 	FC,
+	Fragment,
 	InputHTMLAttributes,
 	TextareaHTMLAttributes,
 } from "react";
-import styles from "./input.style.module.scss";
 
-export const Input: FC<DetailedHTMLProps<
-	InputHTMLAttributes<HTMLInputElement>,
-	HTMLInputElement
->> = (props) => {
-	const { className = "", ...rest } = props;
-	return <input {...rest} className={`${styles.input} ${className}`} />;
+export const Input: FC<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>> = (props) => {
+	const { theme } = useTheme();
+
+	return (
+    <Fragment>
+      <input {...props} />
+
+      <style jsx>{`
+        input {
+          height: 45px;
+          outline: none;
+          border-radius: 6px;
+          margin-bottom: 20px;
+          padding: 5px 10px 5px 10px;
+          border: 1px solid lightgray;
+          color: ${theme.colors.textColor};
+        }
+      `}</style>
+    </Fragment>
+  );
 };
 
 export const TextArea: FC<DetailedHTMLProps<
 	TextareaHTMLAttributes<HTMLTextAreaElement>,
 	HTMLTextAreaElement
 >> = (props) => {
-	const { className = "", ...rest } = props;
-	return <textarea {...rest} className={`${styles.textarea} ${className}`} />;
+	const { theme } = useTheme();
+
+	return (
+    <Fragment>
+      <textarea {...props} />
+
+      <style jsx>{`
+        textarea {
+          padding: 10px;
+          outline: none;
+          display: block;
+          resize: vertical;
+          min-height: 150px;
+          border-radius: 6px;
+          margin-bottom: 25px;
+          font: 400 13.333333px Arial;
+          border: 1px solid lightgray;
+          color: ${theme.colors.textColor};
+        }
+      `}</style>
+    </Fragment>
+  );
 };
