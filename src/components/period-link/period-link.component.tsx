4a7c02747d99d961b55317c3b7af5725fd94390a
emeraldsanto <yanick.belanger@yahoo.com>
2021-01-10 12:34:54 -0500

    Migrated link to styled-jsx

--- src/components/period-link/period-link.component.tsx
+++ src/components/period-link/period-link.component.tsx
@@ -1,12 +1,39 @@
-import { FC } from "react";
-import styles from "./period-link.style.module.scss";
+import { useTheme } from '@contexts/theme/theme-context';
+import { FC, Fragment } from "react";
 
 export const PeriodLink: FC = () => {
+	const { theme } = useTheme();
+
 	return (
-		<div className={styles.container}>
-			<span className={styles.bullet} />
-			<div className={styles.line} />
-			<span className={styles.bullet} />
-		</div>
-	);
+    <Fragment>
+      <div className="container">
+        <span className="bullet" />
+        <div className="line" />
+        <span className="bullet" />
+      </div>
+
+      <style jsx>{`
+				.container {
+					display: flex;
+					align-items: center;
+					flex-direction: column;
+					margin: 10px auto 10px auto;
+				}
+
+				.bullet {
+					width: 8px;
+					height: 8px;
+					border-radius: 50%;
+					background-color: ${theme.colors.textColor};
+				}
+
+				.line {
+					width: 4px;
+					height: 75px;
+					margin: -4px 0 -4px 0;
+					background-color: ${theme.colors.textColor};
+				}
+			`}</style>
+    </Fragment>
+  );
 };
