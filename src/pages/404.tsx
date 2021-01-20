05aab5d3374e807951ff212cb90656b0914696d0
emeraldsanto <yanick.belanger@yahoo.com>
2021-01-19 22:58:45 -0500

    404 to styled-components

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,70 +1,55 @@
 import { Button } from "@components/button/button.component";
 import { Page } from "@components/page/page.component";
-import { combineClassNames } from '@lib/design';
 import { NextPage } from "next";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
-import { Fragment } from 'react';
-import css from 'styled-jsx/css';
+import styled from 'styled-components';
 
 const NotFound: NextPage = () => {
 	const { t } = useTranslation();
-	const buttonStyles = getButtonStyles();
 
 	return (
-    <Fragment>
-      <Page>
-        <div className="container">
-          <h1 className="status">404</h1>
-          <p className="title">{t("404:title")}</p>
-          <p className="catch">{t("404:catch")}</p>
+    <Page>
+      <Container>
+        <Status>404</Status>
+        <Title>{t("404:title")}</Title>
+        <Catch>{t("404:catch")}</Catch>
 
-          <Link href="/">
-            <Button className={combineClassNames("button", buttonStyles)}>
-              <p>{t("404:cta")}</p>
-            </Button>
-          </Link>
-        </div>
-      </Page>
-
-      <style jsx>{`
-        .container {
-          width: 500px;
-        }
-
-        .status {
-          margin-bottom: 10px;
-        }
-
-        .title {
-          font-size: 1.5em;
-        }
-
-        .catch {
-          margin: 20px 0 30px 0;
-          text-align: justify;
-        }
-
-        @media only screen and (max-width: 625px) {
-          .container {
-            width: 100%;
-          }
-        }
-      `}</style>
-
-			{buttonStyles.styles}
-    </Fragment>
+        <Link href="/">
+          <StyledButton>
+            <p>{t("404:cta")}</p>
+          </StyledButton>
+        </Link>
+      </Container>
+    </Page>
   );
 };
 
 export default NotFound;
 
-function getButtonStyles() {
-	return css.resolve`
-    @media only screen and (max-width: 625px) {
-      .button {
-        width: 100%;
-      }
-    }
-  `;
-}
+const Container = styled.div`
+  width: 500px;
+
+  @media only screen and (max-width: 625px) {
+    width: 100%;
+  }
+`;
+
+const Status = styled.h1`
+  margin-bottom: 10px;
+`;
+
+const Title = styled.p`
+  font-size: 1.5em;
+`;
+
+const Catch = styled.p`
+  margin: 20px 0 30px 0;
+  text-align: justify;
+`;
+
+const StyledButton = styled(Button)`
+  @media only screen and (max-width: 625px) {
+    width: 100%;
+  }
+`;
