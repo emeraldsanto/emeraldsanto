66b3a8e135bb1bc4e05b70e8766df92fe9c3fa8c
emeraldsanto <yanick.belanger@yahoo.com>
2021-01-19 21:10:44 -0500

    PeriodLink to styled-components

--- src/components/period-link/period-link.component.tsx
+++ src/components/period-link/period-link.component.tsx
@@ -1,39 +1,33 @@
-import { useTheme } from '@contexts/theme/theme-context';
-import { FC, Fragment } from "react";
+import { FC } from "react";
+import styled from 'styled-components';
 
 export const PeriodLink: FC = () => {
-	const { theme } = useTheme();
-
 	return (
-    <Fragment>
-      <div className="container">
-        <span className="bullet" />
-        <div className="line" />
-        <span className="bullet" />
-      </div>
-
-      <style jsx>{`
-				.container {
-					display: flex;
-					align-items: center;
-					flex-direction: column;
-					margin: 10px auto 10px auto;
-				}
-
-				.bullet {
-					width: 8px;
-					height: 8px;
-					border-radius: 50%;
-					background-color: ${theme.colors.textColor};
-				}
-
-				.line {
-					width: 4px;
-					height: 75px;
-					margin: -4px 0 -4px 0;
-					background-color: ${theme.colors.textColor};
-				}
-			`}</style>
-    </Fragment>
+		<Container>
+			<Bullet />
+			<Line />
+			<Bullet />
+		</Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 10px auto 10px auto;
+`;
+
+const Bullet = styled.span`
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors.textColor};
+`;
+
+const Line = styled.div`
+  width: 4px;
+  height: 75px;
+  margin: -4px 0 -4px 0;
+  background-color: ${(props) => props.theme.colors.textColor};
+`;
