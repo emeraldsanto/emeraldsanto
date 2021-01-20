e8c2907cbe75847604b1a1cc36b7219bfc45420a
emeraldsanto <yanick.belanger@yahoo.com>
2021-01-19 21:08:09 -0500

    Sidebar to styled-components

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -1,15 +1,10 @@
 import { LanguageSwitcher } from '@components/language-switcher/language-switcher.component';
-import { useTheme } from '@contexts/theme/theme-context';
-import {
-	faGithubSquare,
-	faLinkedin,
-	faTwitterSquare
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithubSquare, faLinkedin, faTwitterSquare} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Colors } from '@lib/design';
 import Link from "next/link";
-import { FC, Fragment } from "react";
-import css from 'styled-jsx/css';
+import { FC } from "react";
+import styled from 'styled-components';
 
 const SOCIALS = [
 	{
@@ -30,131 +25,110 @@ const SOCIALS = [
 ];
 
 export const SideBar: FC = () => {
-	const { theme } = useTheme();
-	const switcherStyles = getSwitcherStyles();
-
 	return (
-    <Fragment>
-      <aside className="sidebar">
-        <Link href="/">
-          <div className="initial">
-            <h3>Y</h3>
-          </div>
-        </Link>
+    <Aside>
+      <Link href="/">
+        <InitialWrapper>
+          <Initial>Y</Initial>
+        </InitialWrapper>
+      </Link>
 
-        <LanguageSwitcher className={switcherStyles.className} />
+      <Switcher />
 
-        <div className="routes-container">{/* TODO: ? */}</div>
+      <Routes>{/* TODO: ? */}</Routes>
 
-        <div className="icons-container">
-          {SOCIALS.map((s) => (
-            <a
-              key={s.url}
-              href={s.url}
-              target="_blank"
-              rel="noopener"
-              aria-label={s.name}
-            >
-              <FontAwesomeIcon size="lg" icon={s.icon} className="icon" />
-            </a>
-          ))}
-        </div>
-      </aside>
-
-      <style jsx>{`
-        .sidebar {
-          width: 70px;
-          height: 100%;
-          display: flex;
-          position: fixed;
-          align-items: center;
-          flex-direction: column;
-          padding: 20px 0 20px 0;
-          justify-content: space-between;
-        }
-
-        .sidebar .initial {
-          width: 30px;
-          height: 30px;
-          display: flex;
-          cursor: pointer;
-          border-radius: 6px;
-          align-items: center;
-          transition: all 0.2s;
-          justify-content: center;
-          border: 1px solid ${Colors.SepiaBlack};
-        }
-
-        .sidebar .initial h3 {
-          color: ${Colors.SepiaBlack};
-        }
-
-        .sidebar .initial:hover {
-          border-color: ${theme.colors.accentColor};
-        }
-
-        .sidebar .initial:hover h3 {
-          color: ${theme.colors.accentColor};
-        }
-
-        .sidebar .routes-container {
-          flex: 1;
-          display: flex;
-          align-items: center;
-          flex-direction: column;
-          justify-content: center;
-        }
-
-        .sidebar .icons-container {
-          display: flex;
-          flex-direction: column;
-        }
-
-        .sidebar .icons-container .icon {
-          margin-top: 15px;
-          color: ${Colors.SepiaBlack};
-          transition: color 0.2s;
-        }
-
-        .sidebar .icons-container .icon:hover {
-          color: ${theme.colors.accentColor};
-        }
-
-        @media only screen and (max-width: 625px) {
-          .sidebar {
-            width: 100%;
-            height: 70px;
-            padding: 20px;
-            position: static;
-            flex-direction: row;
-          }
-
-          .sidebar .icons-container {
-            flex-direction: row;
-          }
-
-          .sidebar .icons-container .icon {
-            margin-top: 0;
-            margin-left: 15px;
-          }
-        }
-      `}</style>
-
-      {switcherStyles.styles}
-    </Fragment>
+      <Icons>
+        {SOCIALS.map((s) => (
+          <a
+            key={s.url}
+            href={s.url}
+            target="_blank"
+            rel="noopener"
+            aria-label={s.name}
+          >
+            <FAIcon size="lg" icon={s.icon} />
+          </a>
+        ))}
+      </Icons>
+    </Aside>
   );
 };
 
-function getSwitcherStyles() {
-  return css.resolve`
-    div {
-      margin: 10px 0;
-    }
+const Aside = styled.aside`
+  width: 70px;
+  height: 100%;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px 0 20px 0;
+  justify-content: space-between;
 
-    @media only screen and (max-width: 625px) {
-      div {
-        margin: 0 10px;
-      }
-    }
-  `;
-}
+  @media only screen and (max-width: 625px) {
+    width: 100%;
+    height: 70px;
+    padding: 20px;
+    position: static;
+    flex-direction: row;
+  }
+`;
+
+const InitialWrapper = styled.div`
+  width: 30px;
+  height: 30px;
+  display: flex;
+  cursor: pointer;
+  border-radius: 6px;
+  align-items: center;
+  transition: all 0.2s;
+  justify-content: center;
+  border: 1px solid ${Colors.SepiaBlack};
+
+  &:hover {
+    border-color: ${(props) => props.theme.colors.accentColor};
+  }
+`;
+
+const Initial = styled.h3`
+  color: ${Colors.SepiaBlack};
+
+  &:hover {
+    color: ${(props) => props.theme.colors.accentColor};
+  }
+`;
+
+const Switcher = styled(LanguageSwitcher)`
+  margin: 10px 0;
+
+  @media only screen and (max-width: 625px) {
+    margin: 0 10px;
+  }
+`;
+
+const Routes = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and (max-width: 625px) {
+    flex-direction: row;
+  }
+`;
+
+const FAIcon = styled(FontAwesomeIcon)`
+  margin-top: 15px;
+  transition: color 0.2s;
+  color: ${Colors.SepiaBlack};
+
+  @media only screen and (max-width: 625px) {
+    margin-top: 0;
+    margin-left: 15px;
+  }
+`;
