8a1c75e82ffa24a5f82c63b41b5d9f244ec5743b
Yanick Bélanger <yanick.belanger@yahoo.com>
2021-01-30 13:41:22 -0500

    Release [Develop --> Master]

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -2,12 +2,12 @@
 const environment = {
   services: {
     emailjs: {
-      userId: process.env.EMAIL_JS_USER_ID!,
-      serviceId: process.env.EMAIL_JS_SERVICE_ID!,
-      templateId: process.env.EMAIL_JS_TEMPLATE_ID!,
+      userId: process.env.NEXT_PUBLIC_EMAIL_JS_USER_ID!,
+      serviceId: process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID!,
+      templateId: process.env.NEXT_PUBLIC_EMAIL_JS_TEMPLATE_ID!,
 		},
 		ga: {
-			trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID!,
+			trackingId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID!,
 		}
   },
 };
