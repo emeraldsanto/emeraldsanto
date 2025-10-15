b9a683985c2fd3dd0e1c68bd7df3f188066704c3
Yanick Bélanger <yanick.belanger@yahoo.com>
2025-10-14 23:26:59 -0400

    add tailwind and simplify image optimization config

--- apps/photos/astro.config.ts
+++ apps/photos/astro.config.ts
@@ -1,22 +1,11 @@
 import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
-    imagesConfig: {
-      sizes: [1080],
-      remotePatterns: [
-        {
-          hostname: 'jh6xyt4qgcm2yqdg.public.blob.vercel-storage.com',
-          pathname: '/**',
-          port: '',
-          protocol: 'https',
-        },
-      ],
-    },
-    imageService: true,
-  }),
+  adapter: vercel({ imageService: true }),
+
   env: {
     schema: {
       BLOB_READ_WRITE_TOKEN: envField.string({
@@ -24,5 +13,13 @@ export default defineConfig({
         context: 'server',
       }),
     },
+  },
+
+  image: {
+    domains: ['jh6xyt4qgcm2yqdg.public.blob.vercel-storage.com'],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
