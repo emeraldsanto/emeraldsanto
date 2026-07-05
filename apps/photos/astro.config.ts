fba2c548be3046b7a3090178600dd974a2e26fe9
Yanick Bélanger <yanick.belanger@yahoo.com>
2026-07-04 23:42:56 -0400

    [photos] feat: omitted keystatic admin routes in non-local environments

--- apps/photos/astro.config.ts
+++ apps/photos/astro.config.ts
@@ -4,9 +4,12 @@ import keystatic from '@keystatic/astro';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+const isLocal =
+  !process.env.VERCEL_ENV || process.env.VERCEL_ENV === 'development';
+
 export default defineConfig({
   adapter: vercel({ imageService: false }),
-  integrations: [react(), keystatic()],
+  integrations: isLocal ? [react(), keystatic()] : [],
   vite: {
     plugins: [tailwindcss()],
   },
