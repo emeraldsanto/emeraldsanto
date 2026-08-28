c53b4bcaff23762c26aca22185225f5205d466ad
Yanick Bélanger <me@yan.fyi>
2026-08-28 10:47:53 -0400

    rename layout, add OG tags

--- apps/photos/astro.config.ts
+++ apps/photos/astro.config.ts
@@ -2,15 +2,23 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 import keystatic from '@keystatic/astro';
 import tailwindcss from '@tailwindcss/vite';
+import type { AstroUserConfig } from 'astro';
 import { defineConfig } from 'astro/config';
 
-const isLocal =
-  !process.env.VERCEL_ENV || process.env.VERCEL_ENV === 'development';
-
-export default defineConfig({
+const config: AstroUserConfig = {
   adapter: vercel({ imageService: false }),
-  integrations: isLocal ? [react(), keystatic()] : [],
   vite: {
     plugins: [tailwindcss()],
   },
-});
+};
+
+if (!process.env.VERCEL_ENV || process.env.VERCEL_ENV === 'development') {
+  config.integrations ??= [];
+  config.integrations.push(react(), keystatic());
+}
+
+if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+  config.site = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+}
+
+export default defineConfig(config);
