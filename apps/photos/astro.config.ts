7b4dc5c53130c1ad8b9600b7a2dd4d590e129c81
Yanick Bélanger <yanick.belanger@yahoo.com>
2026-08-28 13:07:35 -0400

    [photos] feat: added OG metadata to `/[category]/[image].astro` (#54)
    ### Summary
    
    This change improves the OG metadata we expose by default, as well as
    adding support for OG image specific to the `/[category]/[image].astro`
    page.

--- apps/photos/astro.config.ts
+++ apps/photos/astro.config.ts
@@ -2,15 +2,27 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 import keystatic from '@keystatic/astro';
 import tailwindcss from '@tailwindcss/vite';
+import type { AstroUserConfig } from 'astro';
 import { defineConfig } from 'astro/config';
 
-const isLocal =
-  !process.env.VERCEL_ENV || process.env.VERCEL_ENV === 'development';
-
-export default defineConfig({
+const config: AstroUserConfig = {
   adapter: vercel({ imageService: false }),
-  integrations: isLocal ? [react(), keystatic()] : [],
   vite: {
     plugins: [tailwindcss()],
   },
-});
+};
+
+switch (process.env.VERCEL_ENV) {
+  case 'production':
+    config.site = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+    break;
+  case 'preview':
+    config.site = `https://${process.env.VERCEL_URL}`;
+    break;
+  default:
+    config.integrations ??= [];
+    config.integrations.push(react(), keystatic());
+    break;
+}
+
+export default defineConfig(config);
